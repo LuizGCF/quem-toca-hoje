64ebec4b06f26c5d89e131aff4a120133a990624
LuizGCF <lgcf97@gmail.com>
2019-04-01 21:08:31 -0300

    Ajustes

---
+++ TCC/app/src/main/java/com/example/quemtocahoje/Utility/ConversaoArquivo.java
@@ -0,0 +1,10 @@
+package com.example.quemtocahoje.Utility;
+
+import com.example.quemtocahoje.POJO.Arquivo;
+
+public class ConversaoArquivo {
+
+    public Byte converterArquivo(Arquivo arquivo){
+        return null;
+    }
+}
