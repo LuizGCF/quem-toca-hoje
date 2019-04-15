537b12299d1abcee0d39e6de0a3d28a7a3443263
LuizGCF <lgcf97@gmail.com>
2019-04-15 20:19:56 -0300

    Várias telas

---
+++ TCC/app/src/main/java/com/example/quemtocahoje/Views/TelaRedefinirSenha.java
@@ -0,0 +1,13 @@
+package com.example.quemtocahoje.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaRedefinirSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_redefinir_senha);
+    }
+}
