0c6eccc559b396057b277141d06877c8101aa486
reisrd <diegoalfareis@gmail.com>
2019-06-07 13:12:47 -0300

    Integração do serviço de Cep

--- TCC/app/src/main/java/com/example/quemtocahoje/Views/TelaEndereco.java
+++ TCC/app/src/main/java/com/example/quemtocahoje/Views/TelaEndereco.java
@@ -7,13 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import com.example.quemtocahoje.Enum.TipoUsuario;
 import com.example.quemtocahoje.Persistencia.Entity.AutenticacaoEntity;
 import com.example.quemtocahoje.Persistencia.Entity.EnderecoEntity;
 import com.example.quemtocahoje.Persistencia.Entity.EstabelecimentoEntity;
 import com.example.quemtocahoje.Utility.DefinirDatas;
 import com.example.quemtocahoje.Utility.Mensagem;
+import com.example.quemtocahoje.Utility.ServicoCEP.Endereco;
+import com.example.quemtocahoje.Utility.ServicoCEP.PostmonService;
 import com.example.tcc.R;
 
 public class TelaEndereco extends AppCompatActivity {
@@ -26,6 +33,12 @@ public class TelaEndereco extends AppCompatActivity {
     private EditText edtUFEndereco;
 
     private Button btnConfirmarEndereco;
+
+    // Mudanças  para buscar cep
+    private Button btnBuscarCep;
+    private Retrofit retrofit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +52,8 @@ public class TelaEndereco extends AppCompatActivity {
         edtComplementoEndereco = findViewById(R.id.edtComplementoEndereco);
         edtCEPEndereco = findViewById(R.id.edtCEPEndereco);
         edtUFEndereco = findViewById(R.id.edtUFEndereco);
+
+
 
         btnConfirmarEndereco = findViewById(R.id.btnConfirmarEndereco);
 
@@ -60,6 +75,55 @@ public class TelaEndereco extends AppCompatActivity {
                     Mensagem.notificar(TelaEndereco.this,"Campos Inválidos","Um ou mais campos não foram preenchidos corretamente.");
             }
         });
+
+
+
+        // Configuração para buscar cep
+
+        btnBuscarCep = findViewById(R.id.btnBuscarCep);
+
+        btnBuscarCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                solicitarEndereco();
+            }
+        });
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.postmon.com.br/v1/cep/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+    }
+
+    private void solicitarEndereco()
+    {
+        String cep = edtCEPEndereco.getText().toString();
+
+        PostmonService service = retrofit.create(PostmonService.class);
+
+        Call<Endereco> call = service.getEndereco(cep);
+
+        call.enqueue(new Callback<Endereco>() {
+            @Override
+            public void onResponse(Call<Endereco> call, Response<Endereco> response) {
+                if (response.isSuccessful()) {
+                    Endereco end = response.body();
+                    edtLogradouroEndereco.setText(end.getLogradouro());
+                    edtBairroEndereco.setText(end.getBairro());
+                    edtCidadeEndereco.setText(end.getCidade());
+                    edtUFEndereco.setText(end.getEstado());
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Endereco> call, Throwable t) {
+                Toast.makeText(TelaEndereco.this, "Não foi possível realizar a requisição", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private boolean isCamposValidos(EditText edtLogradouroEndereco, EditText edtBairroEndereco, EditText edtCidadeEndereco, EditText edtCEPEndereco, EditText edtUFEndereco)
